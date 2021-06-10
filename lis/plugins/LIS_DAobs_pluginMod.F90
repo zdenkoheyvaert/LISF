69f883f22f84ef58c7304f2e941fe9cea08caf06
Michel Bechtold <michel.bechtold@kuleuven.be>
2021-06-10 13:59:37 +0200

    implementation new S1 DA instances for noahmp3.6

--- lis/plugins/LIS_DAobs_pluginMod.F90
+++ lis/plugins/LIS_DAobs_pluginMod.F90
@@ -217,8 +217,20 @@ subroutine LIS_DAobs_plugin
    use SMMRSNWDsnow_Mod,        only : SMMRSNWDsnow_setup
 #endif
 
-#if ( defined DA_OBS_S1_sigma )
-   use S1_sigma_Mod,       only : S1_sigma_setup 
+#if ( defined DA_OBS_S1_sigmaVVSM )
+   use S1_sigmaVVSM_Mod,       only : S1_sigmaVVSM_setup 
+#endif 
+
+#if ( defined DA_OBS_S1_sigmaVVSMLAI )
+   use S1_sigmaVVSMLAI_Mod,       only : S1_sigmaVVSMLAI_setup 
+#endif 
+
+#if ( defined DA_OBS_S1_sigmaVHSM )
+   use S1_sigmaVHSM_Mod,       only : S1_sigmaVHSM_setup 
+#endif 
+
+#if ( defined DA_OBS_S1_sigmaVHSMLAI )
+   use S1_sigmaVHSMLAI_Mod,       only : S1_sigmaVHSMLAI_setup 
 #endif 
 
 #if ( defined DA_OBS_SSMI_SNWD )
@@ -372,8 +384,20 @@ subroutine LIS_DAobs_plugin
    external read_SMMRSNWDsnow, write_SMMRSNWDsnowobs
 #endif
 
-#if ( defined DA_OBS_S1_sigma)    
-   external read_S1_sigma, write_S1_sigmaobs  
+#if ( defined DA_OBS_S1_sigmaVVSM)    
+   external read_S1_sigmaVVSM, write_S1_sigmaVVSMobs  
+#endif 
+
+#if ( defined DA_OBS_S1_sigmaVVSMLAI)    
+   external read_S1_sigmaVVSMLAI, write_S1_sigmaVVSMLAIobs  
+#endif 
+
+#if ( defined DA_OBS_S1_sigmaVHSM)    
+   external read_S1_sigmaVHSM, write_S1_sigmaVHSMobs  
+#endif 
+
+#if ( defined DA_OBS_S1_sigmaVHSMLAI)    
+   external read_S1_sigmaVHSMLAI, write_S1_sigmaVHSMLAIobs  
 #endif 
 
 #if ( defined DA_OBS_SSMI_SNWD )
@@ -616,15 +640,48 @@ subroutine LIS_DAobs_plugin
         write_SMMRSNWDsnowobs)
 #endif
 
-#if ( defined DA_OBS_S1_sigma )
-!S1 backscatter obs
-   call registerdaobsclass(trim(LIS_S1_sigma_obsId),"LSM")
-   call registerdaobssetup(trim(LIS_S1_sigma_obsId)//char(0), &
-        S1_sigma_setup)
-   call registerreaddaobs(trim(LIS_S1_sigma_obsId)//char(0),  &
-        read_S1_sigma)
-   call registerwritedaobs(trim(LIS_S1_sigma_obsId)//char(0), &
-        write_S1_sigmaobs)
+#if ( defined DA_OBS_S1_sigmaVVSM )
+!S1 backscatter obs VVSM
+   call registerdaobsclass(trim(LIS_S1_sigmaVVSM_obsId),"LSM")
+   call registerdaobssetup(trim(LIS_S1_sigmaVVSM_obsId)//char(0), &
+        S1_sigmaVVSM_setup)
+   call registerreaddaobs(trim(LIS_S1_sigmaVVSM_obsId)//char(0),  &
+        read_S1_sigmaVVSM)
+   call registerwritedaobs(trim(LIS_S1_sigmaVVSM_obsId)//char(0), &
+        write_S1_sigmaVVSMobs)
+#endif
+
+#if ( defined DA_OBS_S1_sigmaVVSMLAI )
+!S1 backscatter obs VVSMLAI
+   call registerdaobsclass(trim(LIS_S1_sigmaVVSMLAI_obsId),"LSM")
+   call registerdaobssetup(trim(LIS_S1_sigmaVVSMLAI_obsId)//char(0), &
+        S1_sigmaVVSMLAI_setup)
+   call registerreaddaobs(trim(LIS_S1_sigmaVVSMLAI_obsId)//char(0),  &
+        read_S1_sigmaVVSMLAI)
+   call registerwritedaobs(trim(LIS_S1_sigmaVVSMLAI_obsId)//char(0), &
+        write_S1_sigmaVVSMLAIobs)
+#endif
+
+#if ( defined DA_OBS_S1_sigmaVHSM )
+!S1 backscatter obs VHSM
+   call registerdaobsclass(trim(LIS_S1_sigmaVHSM_obsId),"LSM")
+   call registerdaobssetup(trim(LIS_S1_sigmaVHSM_obsId)//char(0), &
+        S1_sigmaVHSM_setup)
+   call registerreaddaobs(trim(LIS_S1_sigmaVHSM_obsId)//char(0),  &
+        read_S1_sigmaVHSM)
+   call registerwritedaobs(trim(LIS_S1_sigmaVHSM_obsId)//char(0), &
+        write_S1_sigmaVHSMobs)
+#endif
+
+#if ( defined DA_OBS_S1_sigmaVHSMLAI )
+!S1 backscatter obs VHSMLAI
+   call registerdaobsclass(trim(LIS_S1_sigmaVHSMLAI_obsId),"LSM")
+   call registerdaobssetup(trim(LIS_S1_sigmaVHSMLAI_obsId)//char(0), &
+        S1_sigmaVHSMLAI_setup)
+   call registerreaddaobs(trim(LIS_S1_sigmaVHSMLAI_obsId)//char(0),  &
+        read_S1_sigmaVHSMLAI)
+   call registerwritedaobs(trim(LIS_S1_sigmaVHSMLAI_obsId)//char(0), &
+        write_S1_sigmaVHSMLAIobs)
 #endif
 
 #if ( defined DA_OBS_SSMI_SNWD )
