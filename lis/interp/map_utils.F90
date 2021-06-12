fa2f595f81239d747ba03a1a25fb870ad61fc029
Michel Bechtold <michel.bechtold@gmx.de>
2021-06-13 00:00:12 +0200

    Update map_utils.F90
    stripe in parallel mode bug fix

--- lis/interp/map_utils.F90
+++ lis/interp/map_utils.F90
@@ -1123,7 +1123,7 @@ CONTAINS
 
     REAL                         :: deltalat
     REAL                         :: deltalon
-    REAL                         :: lon360
+    REAL                         :: lon360,slon360
 
 
     ! Compute deltalat and deltalon as the difference between the input 
@@ -1138,9 +1138,18 @@ CONTAINS
     ELSE 
       lon360 = lon
     ENDIF    
-    deltalon = lon360 - proj%lon1      
-    IF (deltalon .LT. 0) deltalon = deltalon + 360.
- 
+    ! deltalon = lon360 - proj%lon1      
+    ! IF (deltalon .LT. 0) deltalon = deltalon + 360.     
+    ! MB June,2021: deltalon+360 causes
+    ! bug at edges of subdomains in parallel mode. 
+    ! Correct handling is like done in llij_gauss
+    IF (proj%lon1 .LT. 0) THEN
+       slon360 = proj%lon1 + 360. 
+    ELSE
+       slon360 = proj%lon1
+    ENDIF
+    deltalon = lon360 - slon360
+
     ! Compute i/j
     i = deltalon/proj%dlon + 1.
     j = deltalat/proj%dlat + 1.
